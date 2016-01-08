e45b3133a670750d392fbe06aa2aa0904519bd6d
Swetha Prabakaran <swetha.prabakaran@gmail.com>
2016-01-08 13:17:28 -0500

    update

--- popup.js
+++ popup.js
@@ -1,10 +1,8 @@
 // Copyright (c) 2014 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
-
 /**
  * Get the current URL.
- *
  * @param {function(string)} callback - called when the URL of the current tab
  *   is found.
  */
@@ -15,7 +13,6 @@ function getCurrentTabUrl(callback) {
     active: true,
     currentWindow: true
   };
-
   chrome.tabs.query(queryInfo, function(tabs) {
     // chrome.tabs.query invokes the callback with a list of tabs that match the
     // query. When the popup is opened, there is certainly a window and at least
@@ -23,11 +20,9 @@ function getCurrentTabUrl(callback) {
     // A window can only have one active tab at a time, so the array consists of
     // exactly one tab.
     var tab = tabs[0];
-
     // A tab is a plain object that provides information about the tab.
     // See https://developer.chrome.com/extensions/tabs#type-Tab
     var url = tab.url;
-
     // tab.url is only available if the "activeTab" permission is declared.
     // If you want to see the URL of other tabs (e.g. after removing active:true
     // from |queryInfo|), then the "tabs" permission is required to see their
@@ -35,7 +30,6 @@ function getCurrentTabUrl(callback) {
     console.assert(typeof url == 'string', 'tab.url should be a string');
     callback(url);
   });
-
   // Most methods of the Chrome extension APIs are asynchronous. This means that
   // you CANNOT do something like this:
   //
@@ -45,7 +39,6 @@ function getCurrentTabUrl(callback) {
   // });
   // alert(url); // Shows "undefined", because chrome.tabs.query is async.
 }
-
 /**
  * @param {string} searchTerm - Search term for Google Image search.
  * @param {function(string,number,number)} callback - Called when an image has
@@ -54,8 +47,7 @@ function getCurrentTabUrl(callback) {
  *   The callback gets a string that describes the failure reason.
  */
 function getImageUrl(searchTerm, callback, errorCallback) {
-  // Google image search - 100 searches per day.
-  // https://developers.google.com/image-search/
+  // Google image search - 100 searches per day // https://developers.google.com/image-search/
   var searchUrl = 'https://ajax.googleapis.com/ajax/services/search/images' +
     '?v=1.0&q=' + encodeURIComponent(searchTerm);
   var x = new XMLHttpRequest();
@@ -86,7 +78,14 @@ function getImageUrl(searchTerm, callback, errorCallback) {
   };
   x.send();
 }
-
+function autoCite()
+{
+  alert("citing this page");
+}
+function insertCite()
+{
+  alert("insert your info");
+}
 function renderStatus(statusText) {
   document.getElementById('status').textContent = statusText;
 }
